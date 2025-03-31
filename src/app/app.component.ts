342bde5a9032dcf6b534b81463ace9f51a2cb0f7
Joshua Byrd <joshuabyrd@outlook.com>
2025-03-31 08:15:13 -0400

    add main structure and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+//import all components
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { TopicButtonsComponent } from './components/topic-buttons/topic-buttons.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { MainArticleComponent } from './components/main-article/main-article.component';
+import { KanbanBoardComponent } from './components/kanban-board/kanban-board.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    NavbarComponent,
+    SearchBarComponent,
+    TopicButtonsComponent,
+    CarouselComponent,
+    MainArticleComponent,
+    KanbanBoardComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'the_agora_app';
+  title: string = 'The Agora';
+  subTitle: string = 'Coming soon for all your news needs!';
 }
+
---
+++ src/app/components/topic-buttons/topic-buttons.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topic-buttons',
+  imports: [],
+  templateUrl: './topic-buttons.component.html',
+  styleUrl: './topic-buttons.component.css'
+})
+export class TopicButtonsComponent {
+
+}
