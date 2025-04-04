483e451a8eff0082d1571e1d5c14a17cda5a04b5
Joshua Byrd <joshuabyrd@outlook.com>
2025-04-04 09:32:00 -0400

    Add persistence service

--- src/app/services/persistence.service.ts
+++ src/app/services/persistence.service.ts
@@ -7,4 +7,31 @@ import { Article } from './news.service';
 export class PersistenceService {
 
   constructor() { }
+
+  //a prefix for the JSON objects when they get saved
+  private storagePrefix = "_agora";
+
+  /**
+   * Saves the given array of articles to local storage
+   * @param board the name fo the board, either 'toBeRead' or 'Reading'
+   * @param articles an array of articles to be saved
+   */
+  saveBoardState(board: 'toBeRead' | 'reading', articles: Article[]): void {
+    localStorage.setItem(
+      this.storagePrefix + board,
+      JSON.stringify(articles)
+    );
+  }
+
+  /**
+   * returns an array of Articles if it exists in local storage. Otherwise
+   * returns an empty array
+   * 
+   * @param board the name of the board to get
+   * @returns an array of articles if it is present in storage
+   */
+  loadBoardState(board: 'toBeRead' | 'reading'): Article[] {
+    const data = localStorage.getItem(this.storagePrefix + board);
+    return data? JSON.parse(data): [];
+  }
 }
